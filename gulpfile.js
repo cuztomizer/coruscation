d49f08f479be31bf6f4bf4a18d38c751488f1c9e
Gabriel Jimenez <cuztomiz@gmail.com>
2020-11-10 03:28:00 -0500

    initial setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
+module.exports.default = watch('*.js', clean)
 gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
@@ -8,18 +9,7 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */`)
     )
@@ -31,18 +21,6 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`<!--
 
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 -->`)
     )
@@ -54,18 +32,6 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */`)
     )
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 /*!
 
 =========================================================
-* Paper Kit React - v1.2.0
+* Coruscation - v1.0.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
 =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
 import React from "react";
@@ -21,6 +12,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
+import "assets/css/bootstrap.min.css";
+import "assets/css/paper-kit.css";
+// import "assets/css/paper-kit.min.css";
+// import "assets/css/paper-kit.css.map";
+import "assets/demo/demo.css";
 import "assets/css/bootstrap.min.css";
 import "assets/scss/paper-kit.scss?v=1.2.0";
 import "assets/demo/demo.css?v=1.2.0";
